ad1fdf5c0173386ca0105d085636fe979433b5be
Asmar003 <esmer.ceferova24@gmail.com>
2023-01-25 03:16:13 -0800

    comopleate navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import { Fragment } from "react";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Fragment>
+      <Navbar/>
+    </Fragment>
   );
 }
 
---
+++ client/src/components/navbar/index.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import styled from "./navbar.module.scss";
+
+function Navbar() {
+  return (
+    <header className={styled.navbar}>
+      <div className={styled.navbar__left}>
+        <div className={styled.navbar__left__logo}>
+          <div className={styled.navbar__left__logo__img}>
+            <img
+              src="https://preview.colorlib.com/theme/course/images/logo.png.webp"
+              alt="logo"
+            />
+            <span>course</span>
+          </div>
+        </div>
+        <div className={styled.navbar__left__links}>
+          <div className={styled.navbar__left__links__nav}>
+            <ul>
+              <li>
+                <a href="#1">home</a>
+              </li>
+              <li>
+                <a href="#2">about us</a>
+              </li>
+              <li>
+                <a href="#3">courses</a>
+              </li>
+              <li>
+                <a href="#4">elements</a>
+              </li>
+              <li>
+                <a href="#5">news</a>
+              </li>
+              <li>
+                <a href="#6">contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className={styled.navbar__right}>
+        <img
+          src="https://preview.colorlib.com/theme/course/images/phone-call.svg"
+          alt="tel"
+        />
+        <span>+43 4566 7788 2457</span>
+      </div>
+    </header>
+
+  );
+}
+
+export default Navbar;
